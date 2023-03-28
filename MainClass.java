4a8d5c886e3aca8533a671afac14cc352c4c6b4e
Trojanzaro <anastasiszaro@gmail.com>
2023-03-28 15:15:10 +0300

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,7 +1,26 @@
 import java.io.IOException;
+import java.lang.IllegalArgumentException;
 
 public class MainClass {
     public static void main(String[] args) throws IOException {
-        JavaLSBSteg.hideFileDialog();
+        try {
+            if(args.length < 1 || args.length > 1) {
+                throw new IllegalArgumentException("Only one argument REQUIRED! -h for hiding -e for extracting");
+            } else {
+                switch(args[0]) {
+                    case "-h":
+                        JavaLSBSteg.hideFileDialog();
+                        break;
+                    case "-e":
+                        JavaLSBSteg.extractFileDialog();
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Usage 'java MainClass <option>' option: -h for hiding -e for extracting");
+                }
+            }
+        } catch(Exception e) {
+            javax.swing.JOptionPane.showMessageDialog(null, e.toString());
+            System.exit(-1);
+        }
     }
 }
